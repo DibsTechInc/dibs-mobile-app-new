b364a7d5325c786830471edf69f0656ddb84197c
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-05-05 17:19:46 -0400

    Add timezone awareness to app

--- app/reducers/CurrentDateReducer/index.js
+++ app/reducers/CurrentDateReducer/index.js
@@ -1,11 +1,14 @@
 import moment from 'moment';
 import { handleActions } from 'redux-actions';
 import { setCurrentDate, addDaysToCurrentDate } from '../../actions/CurrentDateActions';
+import Config from '../../../config.json';
+
+const initialState = moment().tz(Config.STUDIO_TZ);
 
 export default handleActions(
   {
     [setCurrentDate]: (state, { payload }) => payload,
-    [addDaysToCurrentDate]: (state, { payload }) => moment(state.add(payload, 'days')),
+    [addDaysToCurrentDate]: (state, { payload }) => moment(state).add(payload, 'days'),
   },
-  moment().local().startOf('day')
+  initialState
 );
