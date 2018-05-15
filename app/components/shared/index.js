3a7465d16eec1e543f0745adcbd3f452eed8ba00
Benjamin Soung <soungbenjamin@gmail.com>
2018-05-15 12:33:42 -0400

    moved purchaseInfo components to shared

--- app/components/shared/index.js
+++ app/components/shared/index.js
@@ -10,3 +10,4 @@ export { default as UpcomingEventSlider } from './UpcomingEventSlider';
 export { default as UpcomingClasses } from './UpcomingClasses';
 export { default as TransactionBreakdown } from './TransactionBreakdown';
 export { default as CustomStatusBar } from './CustomStatusBar';
+export { default as PaymentInfo } from './PaymentInfo';
