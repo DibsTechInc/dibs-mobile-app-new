bfca0dacb8171f46af407e50b13028a506f71679
Benjamin Soung <soungbenjamin@gmail.com>
2018-05-21 19:05:31 -0400

    signup qa

--- app/components/AuthPage/Signup.js
+++ app/components/AuthPage/Signup.js
@@ -8,6 +8,7 @@ import {
   View,
   ScrollView,
 } from 'react-native';
+import { CheckBox } from 'react-native-elements'
 import styled from 'styled-components';
 import { KeyboardAccessoryView } from 'react-native-keyboard-accessory';
 import Promise from 'bluebird';
@@ -16,7 +17,6 @@ import { MaterialButton, CustomStatusBar, FadeInView, InputField } from '../shar
 import { TERMS_AND_CONDITIONS_ROUTE, MAIN_ROUTE, LOGIN_ROUTE, DEFAULT_BG } from '../../constants';
 import Config from '../../../config.json';
 import DibsLoader from '../shared/DibsLoader';
-import { CheckBox } from 'react-native-elements'
 
 const StyledButtonView = styled.View`
   padding: 8px;
@@ -159,17 +159,17 @@ class Signup extends Component {
             onChangeText={password => this.setState({ password })}
             placeholder="Password (6 char min)"
             style={{ width: 250 }}
-            containerStyle={{ marginBottom: 25 }}
+            containerStyle={{ marginBottom: 30 }}
           />
-          <View style={{ width: 250, justifyContent: 'space-between', marginBottom: 20 }}>
-            <TouchableOpacity onPress={() => this.props.navigation.navigate(TERMS_AND_CONDITIONS_ROUTE, { url: Config.STUDIO_TERMS_LINK })}>
+          <View style={{ width: 250, justifyContent: 'space-between', marginBottom: 10 }}>
+            <TouchableOpacity style={{ marginBottom: 10 }} onPress={() => this.props.navigation.navigate(TERMS_AND_CONDITIONS_ROUTE, { url: Config.STUDIO_TERMS_LINK })}>
               <Text style={{ color: Config.STUDIO_COLOR, fontFamily: 'flex-font' }}>Flex Studios Terms</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => this.props.navigation.navigate(TERMS_AND_CONDITIONS_ROUTE, { url: Config.DIBS_TERMS_LINK })}>
               <Text style={{ color: Config.STUDIO_COLOR, fontFamily: 'flex-font' }}>Dibs Terms</Text>
             </TouchableOpacity>
           </View>
-          <View style={{ width: 250, height: 50, position: 'relative' }}>
+          <View style={{ width: 250, height: 30, position: 'relative', marginTop: 35 }}>
             <CheckBox
               title="I have read and agreed to the terms"
               checked={this.state.tAndC}
