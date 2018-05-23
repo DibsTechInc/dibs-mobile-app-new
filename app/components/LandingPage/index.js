cefc83fbdf259b75f4ae22482d9c893d0a12ed5a
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-05-23 17:08:16 -0400

    remove conflict markers

--- app/components/LandingPage/index.js
+++ app/components/LandingPage/index.js
@@ -11,11 +11,7 @@ import styled from 'styled-components';
 import Swiper from 'react-native-swiper';
 
 import About from './About';
-<<<<<<< HEAD
-import { DARK_TEXT_GREY, VERIFY_ROUTE } from '../../constants';
-=======
 import { VERIFY_ROUTE, DARK_TEXT_GREY } from '../../constants';
->>>>>>> 5e3c7828ba6ced8abdf5155ef04a3b94ff787f6b
 import Config from '../../../config.json';
 import { FadeInView, CustomStatusBar, MaterialButton } from '../shared';
 import { FlexCenter, NormalText } from '../styled';
