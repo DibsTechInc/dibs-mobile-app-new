fe53f94fb42af19d0d551a3edee030a6e2d3440e
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-05-24 19:33:06 -0400

    pulse animation on notification icon

--- app/components/shared/Notification.js
+++ app/components/shared/Notification.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { View, Animated } from 'react-native';
+
 import { WHITE, RED } from '../../constants';
 import { NormalText } from '../styled';
 
-const StyledView = styled.View`
+const RelativeView = styled.View`
   position: relative;
 `;
 
-// default props are from the use case
-// i abstracted this out of
+const notificationStyle = props => ({
+  alignItems: 'center',
+  backgroundColor: props.backgroundColor,
+  borderRadius: props.radius,
+  justifyContent: 'center',
+  height: 2 * props.radius,
+  marginRight: props.marginRight,
+  position: 'absolute',
+  right: props.right,
+  top: props.top,
+  width: 2 * props.radius,
+  zIndex: 1,
+});
 
-const StyledNotification = styled.View`
-  justify-content: center;
-  align-items: center;
-  background-color: ${props => props.backgroundColor};
-  z-index: 1;
-  border-radius: ${props => props.radius};
-  margin-right: ${props => props.marginRight};
-  width: ${props => (2 * props.radius)};
-  height: ${props => (2 * props.radius)};
-  position: absolute;
-  right: ${props => props.right};
-  top: ${props => props.top};
-`;
-
-const StyledNotificationText = NormalText.extend`
+const NotificationText = NormalText.extend`
   font-size: ${props => props.fontSize};
   color: ${props => props.notificationTextColor};
 `;
+
+const getRadiusValue = (radius, animValue) => Animated.add(radius, Animated.multiply(radius, animValue));
+const getDiameterValue = (radius, animValue) => Animated.multiply(2, getRadiusValue(radius, animValue));
+const getPositionValue = (pos, radius, animValue) => Animated.add(pos, Animated.multiply(-radius, animValue));
 
 /**
  * @class Notification
@@ -36,19 +39,60 @@ const StyledNotificationText = NormalText.extend`
  */
 export default class Notification extends React.PureComponent {
   /**
+   * @constructor
+   * @constructs Notification
+   * @param {Object} props for component
+   */
+  constructor(props) {
+    super(props);
+    this.state = { showPulse: false, animValue: new Animated.Value(0) };
+  }
+  /**
+   * @param {Object} props component will receive
+   * @returns {undefined}
+   */
+  async componentWillReceiveProps(props) {
+    if (props.notificationCount && props.notificationCount !== this.props.notificationCount) {
+      await new Promise(res => this.setState({ showPulse: true }, res));
+      await new Promise(res => Animated.timing(
+        this.state.animValue,
+        {
+          toValue: 1,
+          duration: 300,
+        }
+      ).start(res));
+      await new Promise(res => this.setState({ animValue: new Animated.Value(0), showPulse: false }, res));
+    }
+  }
+  /**
  * @returns {JSX} XML
  */
   render() {
     if (!this.props.notificationCount) return this.props.children;
     return (
-      <StyledView>
-        <StyledNotification {...this.props}>
-          <StyledNotificationText {...this.props}>
+      <RelativeView>
+        {this.state.showPulse && (
+          <Animated.View
+            style={[
+              notificationStyle(this.props),
+              {
+                borderRadius: getRadiusValue(this.props.radius, this.state.animValue),
+                height: getDiameterValue(this.props.radius, this.state.animValue),
+                width: getDiameterValue(this.props.radius, this.state.animValue),
+                opacity: Animated.add(1, Animated.multiply(-1, this.state.animValue)),
+                top: getPositionValue(this.props.top, this.props.radius, this.state.animValue),
+                right: getPositionValue(this.props.right, this.props.radius, this.state.animValue),
+              },
+            ]}
+          />
+        )}
+        <View style={notificationStyle(this.props)}>
+          <NotificationText {...this.props}>
             {this.props.notificationCount}
-          </StyledNotificationText>
-        </StyledNotification>
+          </NotificationText>
+        </View>
         {this.props.children}
-      </StyledView>
+      </RelativeView>
     );
   }
 }
@@ -75,4 +119,5 @@ Notification.propTypes = {
     PropTypes.number,
   ]),
   children: PropTypes.element,
+  top: PropTypes.number,
 };
