a653a5b30fa450d0fda4928f7ec2a6a37f714267
Benjamin Soung <soungbenjamin@gmail.com>
2018-05-31 13:31:42 -0400

    fixed logout button not appearing in prod app

--- app/helpers/generateQuote.js
+++ app/helpers/generateQuote.js
@@ -24,10 +24,20 @@ const quotes = [
   'The body achieves what the mind believes.',
 ];
 
+let previousIndex = null;
+
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const randomIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  if (previousIndex !== randomIndex) {
+    previousIndex = randomIndex;
+  } else {
+    getRandomIntInclusive(min, max);
+  }
+
+  return randomIndex;
 }
 
 export default function generateQuote() {
