6915100ed3bdf195ae05132b116e92a3282da199
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-07-05 12:51:56 -0400

    Automate pushing updates to multiple apps by running a NodeJS script (#47)
    * starting automated update publish script
    
    * replace files w assets from s3
    
    * update script finished, could be tested more
    
    * only update prod channels of studios w live apps

---
+++ bin/update.js
@@ -0,0 +1,131 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const config = require('./config');
+const program = require('commander');
+const Sequelize = require('sequelize');
+const Promise = require('bluebird');
+const fs = require('fs');
+const path = require('path');
+const AWS = require('aws-sdk');
+const { spawn } = require('child_process');
+
+const ALL_STUDIOS_QUERY =
+  'SELECT app_json AS "appJson", app_config_json AS "configJson" FROM dibs_studios WHERE app_json, app_config_json IS NOT NULL AND (NOT $prod OR "liveMobileApp");';
+const STUDIO_QUERY =
+  'SELECT app_json AS "appJson", app_config_json AS "configJson" FROM dibs_studios WHERE id = $id;';
+
+const APP_ROOT = path.join(__dirname, '..');
+const S3_BUCKET = 'dibs-mobile-assets';
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, config);
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+const s3 = new AWS.S3();
+const getObjectAsync = Promise.promisify(s3.getObject, { context: s3 });
+
+/**
+ * @param {Object} SQL query result which contains app.json and config.json
+ * @returns {Object} result of the publish for a particular studio
+ */
+async function updateStudioApp({ appJson, configJson }) {
+  try {
+    const getS3Object = key => getObjectAsync({
+      Bucket: S3_BUCKET,
+      Key: `${configJson.S3_DIRNAME}/${key}`,
+    });
+    const writeFile = (filename, data) => fs.writeFileSync(
+      path.join(APP_ROOT, filename),
+      data
+    );
+
+    // Overwriting config files
+    fs.writeFileSync(
+      path.join(APP_ROOT, '/app.json'),
+      new Buffer(JSON.stringify(appJson)));
+    fs.writeFileSync(
+      path.join(APP_ROOT, '/config.json'),
+      new Buffer(JSON.stringify(configJson)));
+
+    // Overwriting fonts
+    const regularFont = await getS3Object(`${configJson.STUDIO_FONT}-Regular.ttf`);
+    const boldFont = await getS3Object(`${configJson.STUDIO_FONT}-Bold.ttf`);
+    writeFile('/assets/fonts/Regular.ttf', regularFont.Body);
+    writeFile('/assets/fonts/Bold.ttf', boldFont.Body);
+
+    // Overwriting assets
+    const iconImg = await getS3Object('icon.png');
+    const splashImg = await getS3Object('splash.png');
+    const mainPageImg = await getS3Object('main-page.png');
+    writeFile('/assets/icon.png', iconImg.Body);
+    writeFile('/assets/splash.png', splashImg.Body);
+    writeFile('/assets/img/main-page.png', mainPageImg.Body);
+
+    // Publish update
+    const releaseChannel = configJson[`RELEASE_CHANNEL_${program.prod ? 'PROD' : 'DEV'}`];
+    await new Promise((resolve, reject) => {
+      const publish = spawn('exp', ['publish', '--release-channel', releaseChannel]);
+      publish.stdout.on('data', data => console.log(data.toString()));
+      publish.on('exit', code => (code ?
+        reject(new Error(`Failed to publish the app for studio ${configJson.DIBS_STUDIO_ID}`))
+        : resolve()));
+    });
+    return { success: true };
+  } catch (err) {
+    console.log('\nERROR PUBLISHING UPDATE:');
+    console.log(`Failed to update app for studio ${configJson.DIBS_STUDIO_ID}:\n`);
+    console.log(err);
+    return { success: false, dibs_studio_id: configJson.DIBS_STUDIO_ID };
+  }
+}
+
+(async function updateApp() {
+  try {
+    program
+      .option('-a, --all', 'Update all studios')
+      .option('-s, --studios <dibs_studio_ids>', 'Studio ID to update', parseInt)
+      .option('-p, --prod', 'Publishes app build to production release channels')
+      .parse(process.argv);
+    if (!program.all && !program.studios) {
+      console.log('You must provide either the --all or --studios option');
+      process.exit(1);
+    }
+
+    const configs = await sequelize.query(
+      ...(program.all ? [
+        ALL_STUDIOS_QUERY,
+        { bind: { prod: Boolean(program.prod) },
+          type: sequelize.QueryTypes.SELECT },
+      ] : [
+        STUDIO_QUERY,
+        { bind: { id: program.studios },
+          type: sequelize.QueryTypes.SELECT },
+      ])
+    );
+
+    const results = await Promise.map(
+      configs,
+      updateStudioApp.bind(null, program),
+      { concurrency: 1 }
+    );
+
+    const successfulPublishes = results.filter(r => r.success);
+    const failedPublishes = results.filter(r => !r.success);
+
+    if (successfulPublishes.length === results.length) {
+      console.log('\nThe update published to each studio app successfully!');
+    } else if (successfulPublishes.length && results.length > 1) {
+      console.log(`\nSuccessfully published the update to ${successfulPublishes.length}/${results.length} apps.`);
+      console.log(`Failed to publish updates to studios: ${failedPublishes.map(r => r.dibs_studio_id).join(', ')}\nSee errors above for details.`);
+    } else {
+      console.log('\nFailed to publish the update to each app. See error(s) above for details.');
+    }
+
+    process.exit(0);
+  } catch (err) {
+    console.log('\nERROR PUBLISHING UPDATE:\n');
+    console.log(err);
+    process.exit(1);
+  }
+}());
