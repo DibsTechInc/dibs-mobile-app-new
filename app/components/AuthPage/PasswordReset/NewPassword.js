b952f621262c785afe54eec57d369903acff1a6d
Benjamin Soung <soungbenjamin@gmail.com>
2018-07-26 11:43:38 -0400

    fix keyboard blocking input on password reset (min 6 chars)

--- app/components/AuthPage/PasswordReset/NewPassword.js
+++ app/components/AuthPage/PasswordReset/NewPassword.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { connect } from 'react-redux';
 
 import { enqueueUserError } from '../../../actions';
@@ -45,6 +46,11 @@ class NewPassword extends React.PureComponent {
    * @returns {undefined}
    */
   onSubmit() {
+    if (!this.state.password.length || this.state.password.length < 6) {
+      this.props.enqueueUserError({ title: 'Error!', message: 'Your password must be at least 6 characters' });
+      return;
+    }
+
     if (this.state.password !== this.state.passwordConfirmation) {
       this.props.enqueueUserError({ title: 'Error!', message: 'The password confirmation does not match!' });
       return;
@@ -59,36 +65,41 @@ class NewPassword extends React.PureComponent {
     return (
       <FadeInView>
         <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-          <FlexCenter style={{ width: '100%' }}>
-            <InputField
-              customFocus
-              autoCapitalize="none"
-              onChangeText={this.onPasswordChange}
-              label="Enter your new password"
-              secureTextEntry
-              containerStyle={{ marginBottom: 30, width: 200, minWidth: 200 }}
-              labelStyle={{ marginBottom: 20 }}
-              placeholder="New password"
-            />
-            <InputField
-              customFocus
-              autoCapitalize="none"
-              onChangeText={this.onPasswordConfirmationChange}
-              label="Confirm your new password"
-              secureTextEntry
-              containerStyle={{ marginBottom: 80, width: 200, minWidth: 200 }}
-              labelStyle={{ marginBottom: 20 }}
-              placeholder="Confirm new password"
-              onSubmitEditing={this.onSubmit}
-            />
-            <ButtonContainer>
-              <MaterialButton
-                onPress={this.onSubmit}
-                text="Submit"
-                style={{ width: 200, height: 40 }}
+          <KeyboardAwareScrollView
+            contentContainerStyle={{ flex: 1 }}
+            resetScrollToCoords={{ x: 0, y: 0 }}
+          >
+            <FlexCenter style={{ width: '100%' }}>
+              <InputField
+                customFocus
+                autoCapitalize="none"
+                onChangeText={this.onPasswordChange}
+                label="Enter your new password"
+                secureTextEntry
+                containerStyle={{ marginBottom: 30, width: 200, minWidth: 200 }}
+                labelStyle={{ marginBottom: 20 }}
+                placeholder="New password (6 char min)"
               />
-            </ButtonContainer>
-          </FlexCenter>
+              <InputField
+                customFocus
+                autoCapitalize="none"
+                onChangeText={this.onPasswordConfirmationChange}
+                label="Confirm your new password"
+                secureTextEntry
+                containerStyle={{ marginBottom: 80, width: 200, minWidth: 200 }}
+                labelStyle={{ marginBottom: 20 }}
+                placeholder="Confirm new password"
+                onSubmitEditing={this.onSubmit}
+              />
+              <ButtonContainer>
+                <MaterialButton
+                  onPress={this.onSubmit}
+                  text="Submit"
+                  style={{ width: 200, height: 40 }}
+                />
+              </ButtonContainer>
+            </FlexCenter>
+          </KeyboardAwareScrollView>
         </TouchableWithoutFeedback>
       </FadeInView>
     );
@@ -97,6 +108,7 @@ class NewPassword extends React.PureComponent {
 
 NewPassword.propTypes = {
   onSubmit: PropTypes.func.isRequired,
+  enqueueUserError: PropTypes.func,
 };
 
 // const mapStateToProps = state => ({});
