c48327fe9374f23928d4ace8a415626a76c6cd59
Dylan Cutler <DCtheTall@users.noreply.github.com>
2018-07-27 10:13:47 -0700

    Fix user reducer to not overwrite other props in state (#57)

--- app/reducers/UserReducer/index.js
+++ app/reducers/UserReducer/index.js
@@ -2,5 +2,5 @@ import { handleActions } from 'redux-actions';
 import { setUser } from '../../actions/UserActions';
 
 export default handleActions({
-  [setUser]: (state, { payload }) => payload,
+  [setUser]: (state, { payload }) => ({ ...state, ...payload }),
 }, {});
