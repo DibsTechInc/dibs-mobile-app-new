043429528d3921ccd4504bbcb500f3875232a198
Benjamin Soung <soungbenjamin@gmail.com>
2018-08-02 15:11:42 -0400

    remove unused contact us component

--- app/components/ProfilePage/index.js
+++ app/components/ProfilePage/index.js
@@ -2,7 +2,6 @@ export { default as ProfilePage } from './ProfilePage';
 export { default as FAQ } from './FAQ';
 export { default as UserSettings } from './UserSettings';
 export { default as AboutUs } from './AboutUs';
-export { default as ContactUs } from './ContactUs';
 export { default as EditUserName } from './EditUserName';
 export { default as EditEmail } from './EditEmail';
 export { default as EditPassword } from './EditPassword';
